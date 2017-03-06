3441e5f4ae5735e6b93279baa4d039beac1cadd6
Samuel Toloza <tlz.system@gmail.com>
2017-03-06 16:44:32 -0300

    responsive

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 
-
+@method_decorator(login_required, name='dispatch')
 class DashboardView(TemplateView):
     val = 1
     template_name ="dashboard/index.html"
