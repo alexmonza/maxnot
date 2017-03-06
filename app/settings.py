d4b62cf4179bb331e4eb5b4e010153be84bdb7d9
Samuel Toloza <tlz.system@gmail.com>
2017-03-06 16:12:49 -0300

    error

--- app/settings.py
+++ app/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT='staticfiles'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-
-]
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 
